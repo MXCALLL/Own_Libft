c2594d0e2bba23829593e3442bcc498093dd0842
MXCALL <idbellam12@gmail.com>
2024-10-07 03:13:37 +0000

    add ft_memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: MXCALL <MXCALL@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 00:22:02 by MXCALL            #+#    #+#             */
+/*   Updated: 2024/10/07 03:12:51 by MXCALL           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*src_ptr;
+	unsigned char	*dest_ptr;
+
+	src_ptr = (unsigned char *)src;
+	dest_ptr = (unsigned char *)dest;
+	i = 0;
+	while (i < n)
+	{
+		dest_ptr[i] = src_ptr[i];
+		i++;
+	}
+	return (dest);
+}
