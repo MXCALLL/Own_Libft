9725cf111d7f2242e4bdb8120e79e34a801851eb
Mustapha Idbella <muidbell@e3r11p7.1337.ma>
2024-11-14 12:43:19 +0100

    Update

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: muidbell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 15:00:24 by muidbell          #+#    #+#             */
-/*   Updated: 2024/11/12 17:28:37 by muidbell         ###   ########.fr       */
+/*   Updated: 2024/11/14 12:36:00 by muidbell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,8 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (s1_ptr[i] > s2_ptr[i])
-			return (1);
-		if (s1_ptr[i] < s2_ptr[i])
-			return (-1);
+		if (s1_ptr[i] != s2_ptr[i])
+			return (s1_ptr[i] - s2_ptr[i]);
 		i++;
 	}
 	return (0);
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: muidbell <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 09:50:56 by muidbell          #+#    #+#             */
-/*   Updated: 2024/11/12 21:39:54 by muidbell         ###   ########.fr       */
+/*   Updated: 2024/11/14 12:30:29 by muidbell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
 	{
-		if (s1[i] > s2[i])
-			return (1);
-		if (s1[i] < s2[i])
-			return (-1);
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
